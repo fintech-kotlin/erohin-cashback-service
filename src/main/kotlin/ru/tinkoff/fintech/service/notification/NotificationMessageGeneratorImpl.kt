2c0a5e5b456046d7086113bd652d16483273ce53
ae <aaaerokhin@ya.ru>
2020-02-28 15:36:14 +0300

    fix generateMessage() to use trimIndent()

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -8,10 +8,12 @@ class NotificationMessageGeneratorImpl(
 
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
         var cardNumber = cardNumberMasker.mask(notificationMessageInfo.cardNumber)
-        return "Уважаемый, ${notificationMessageInfo.name}!\n" +
-                "Спешим Вам сообщить, что на карту $cardNumber\n" +
-                "начислен cashback в размере ${notificationMessageInfo.cashback}\n" +
-                "за категорию ${notificationMessageInfo.category}.\n" +
-                "Спасибо за покупку ${notificationMessageInfo.transactionDate}"
+        return """
+                Уважаемый, ${notificationMessageInfo.name}!
+                Спешим Вам сообщить, что на карту $cardNumber
+                начислен cashback в размере ${notificationMessageInfo.cashback}
+                за категорию ${notificationMessageInfo.category}.
+                Спасибо за покупку ${notificationMessageInfo.transactionDate}
+        """.trimIndent()
     }
 }
